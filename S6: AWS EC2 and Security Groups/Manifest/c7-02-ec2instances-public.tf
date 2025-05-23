d9d8fc3030ddd254c5e8d885c3f0e553e9d2dbfb
Julián <134458575+julianzanetti@users.noreply.github.com>
2025-05-22 21:06:22 -0300

    Update c7-02-ec2instances-public.tf

--- S6: AWS EC2 and Security Groups/Manifest/c7-02-ec2instances-public.tf
+++ S6: AWS EC2 and Security Groups/Manifest/c7-02-ec2instances-public.tf
@@ -3,10 +3,10 @@ module "public_ec2_instance" {
   version = "5.8.0"
 
   name = "${var.environment}-Public-Instance"
-  ami = data.aws_ami.amzlinux2
+  ami = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instace_key
   vpc_security_group_ids = [module.public-security-group.security_group_id]
-  subnet_id              = [module.vpc.public_subnets]
+  subnet_id              = module.vpc.public_subnets[0]
   tags = local.commom_tags
 }

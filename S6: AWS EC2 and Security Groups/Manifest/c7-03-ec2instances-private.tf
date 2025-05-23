1201bf74bd0e1bdacf7bf8a80507833479597923
Julián <134458575+julianzanetti@users.noreply.github.com>
2025-05-22 21:06:06 -0300

    Update c7-03-ec2instances-private.tf

--- S6: AWS EC2 and Security Groups/Manifest/c7-03-ec2instances-private.tf
+++ S6: AWS EC2 and Security Groups/Manifest/c7-03-ec2instances-private.tf
@@ -5,12 +5,12 @@ module "private_ec2_instance" {
   depends_on = [module.vpc]
   for_each = toset(["1", "2"])
   name = "${var.environment}-PrivateInstance-${each.key}"
-  ami = data.aws_ami.amzlinux2
+  ami = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instace_key
   user_data = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [module.private-security-group.security_group_id]
-  subnet_id              = element([module.vpc.private_subnets], tonumber(each.key))
+  subnet_id              = element(module.vpc.private_subnets, tonumber(each.key))
 
   tags = local.commom_tags
 }
